72fc2ffcf541d2bdc4fe07a8e10e33589a4a24fc
mattor1000 <aoj219@lehigh.edu>
2015-08-31 18:54:59 +0000

    update WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,14 @@
+//Alex James
+//9-02-2015
+//CSE002
+public class WelcomeClass{
+	public static void main(String[] args){
+		System.out.println("  ------------");
+		System.out.println("  |  WELCOME |");
+		System.out.println("  ------------");
+		System.out.println("  ^  ^  ^  ^  ^  ^");
+		System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+		System.out.println("|-A--O--J--2--1--9-|");
+		System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+		System.out.println("  v  v  v  v  v  v");
+		System.out.println("I am from Hillside, NJ and I love to bowl.");}}
